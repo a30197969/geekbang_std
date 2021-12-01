0cae560262e0f963558791a1c2c0084f5cd9a2a5
peng.liang <peng.liang@fengniao.com>
2021-12-01 19:42:08 +0800

    both stream

---
+++ grpc_both_stream/server/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"geekbang_study/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	Address string = ":8000"
+	Network string = "tcp"
+)
+
+func main() {
+	// 监听本地端口
+	listener, err := net.Listen(Network, Address)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v\n", err)
+	}
+	log.Println(Address + " net.Listing...")
+	// 新建gRPC服务器实例
+	grpcServer := grpc.NewServer()
+	// 在gRPC服务器注册我们的服务
+	proto.RegisterStreamServer(grpcServer, &StreamService{})
+	// 用服务器 Serve() 方法以及我们的端口信息区实现阻塞等待，直到进程被杀死或者 Stop() 被调用
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v\n", err)
+	}
+}
