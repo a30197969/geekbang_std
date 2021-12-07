11abf3a8995ab7b42564b77f8e07e6d0db4d4342
peng.liang <peng.liang@fengniao.com>
2021-12-07 18:40:10 +0800

    数据库连接测试

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module geekbang_study
 go 1.17
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/pkg/errors v0.9.1
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 	golang.org/x/text v0.3.0 // indirect
--- homework/first/error_test.go
+++ homework/first/error_test.go
@@ -1,15 +1,32 @@
 package first
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
 	"testing"
 )
 
-func TestError(t *testing.T) {
-	err := errors.New("这是一个错误")
-	err2 := errors.New("这是一个错误")
-	fmt.Printf("%#v\n", err)
-	fmt.Printf("%p\n", err)
-	fmt.Printf("%p\n", err2)
+func TestQueryRow(t *testing.T) {
+	title, err := GetAlbumTitle(13)
+	fmt.Printf("%+v,%+v\n", title, err)
+}
+func GetAlbumTitle(userid int) (string, error) {
+	db, err := sql.Open("mysql", "fengniao:fengniao123@tcp(172.16.151.61:3306)/fn_bird")
+	if err != nil {
+		return "", errors.Wrap(err, "连接 iam 库报错")
+	}
+	defer db.Close()
+
+	var title string
+	err = db.QueryRow("select title from `album` where userid = ?", userid).Scan(&title)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", nil
+		} else {
+			return "", errors.Wrap(err, "查询 fn_bird.album 表报错")
+		}
+	}
+	return title, nil
 }
