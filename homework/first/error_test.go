56edaf8166ec90572a762a5ed16e64c7ebed4a45
peng.liang <peng.liang@fengniao.com>
2021-12-08 11:25:54 +0800

    日常练习

--- homework/first/error_test.go
+++ homework/first/error_test.go
@@ -1,11 +1,10 @@
 package first
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestQueryRow(t *testing.T) {
 	row, err := GetQueryRow(12)
-	fmt.Printf("%+v,%+v\n", row, err)
+	t.Logf("%+v,%+v\n", row, err)
 }
