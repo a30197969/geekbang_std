e7b32ff0613bc03c69e13201731bd326a6ca8e2c
peng.liang <peng.liang@fengniao.com>
2021-12-15 16:06:19 +0800

    22

--- base/goroutine/goroutine_test.go
+++ base/goroutine/goroutine_test.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
+type config struct {
+	a []int
+}
+
+func (c *config) T() {
+
+}
+
 func TestGoroutine(t *testing.T) {
-	done := make(chan error, 2)
-	fmt.Println(cap(done))
+	var v atomic.Value
+	v.Store(&config{})
+	go func() {
+		i := 0
+		for {
+			i++
+			cfg := &config{a: []int{i, i + 1, i + 2, i + 3, i + 4, i + 5}}
+			v.Store(cfg)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 2; j++ {
+				cfg := v.Load().(*config)
+				cfg.T()
+				fmt.Printf("%v\n", cfg)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
 }
