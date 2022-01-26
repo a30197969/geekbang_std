32e2d824ce959849c7579f390cf0773fbea22f54
peng.liang <peng.liang@fengniao.com>
2022-01-26 18:31:59 +0800

    no message

---
+++ network/tcp/proto.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// Encode 将消息编码
+func Encode(message string) ([]byte, error) {
+	// 读取消息的长度，转换成int32类型（占4个字节）
+	var length = int32(len(message))
+	var pkg = new(bytes.Buffer) // 缓冲区
+	err := binary.Write(pkg, binary.LittleEndian, length)
+	if err != nil {
+		return nil, err
+	}
+	// 写入消息实体
+	err = binary.Write(pkg, binary.LittleEndian, []byte(message))
+	if err != nil {
+		return nil, err
+	}
+	return pkg.Bytes(), nil
+}
+
+// Decode 将消息解码
+func Decode() {
+
+}
